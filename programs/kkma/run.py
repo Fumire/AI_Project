a9234300180f086ac841444497a960033bcaa626
Jaewoong Lee <230@fumire.moe>
2020-05-05 11:36:01 +0900

    renamed directory

--- programs/kkma/run.py
+++
@@ -1,7 +0,0 @@
-import time
-import gtts
-import konlpy.tag
-import pymysql
-
-kkma = konlpy.tag.Kkma()
-print(kkma.pos(input("Enter: "), join=True))
---
+++ programs/okt/run.py
@@ -0,0 +1,30 @@
+import time
+import gtts
+import konlpy.tag
+import pymysql
+
+
+def okt_token(text):
+    okt = konlpy.tag.Okt()
+    answer = ""
+    for word, pumsa in okt.pos(text, norm=True):
+        if pumsa == "Josa":
+            answer += word
+        elif pumsa == "Punctuation":
+            answer += word
+        else:
+            answer += " " + word
+    return answer
+
+
+with open("password.txt", "r") as f:
+    password = f.readline().strip()
+
+# connection = pymysql.connect(host="fumire.moe", user="fumiremo_remote", password=password, db="fumiremo_AI", charset="utf8", port=3306)
+# cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+# query = "SELECT * FROM `SpeechList` WHERE `Algorithm` LIKE 'kkma' AND `Status` LIKE 'wait' ORDER BY `UploadTime` ASC"
+# cursor.execute(query)
+
+tts = gtts.gTTS(okt_token(input("Enter:")), lang="ko-KR", lang_check=False)
+tts.save("/data/tmp.mp3")
