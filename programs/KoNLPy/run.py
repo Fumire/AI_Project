aefd23bb2df50cdf49d29c208514675eff2ed114
Jaewoong Lee <230@fumire.moe>
2020-05-06 19:28:39 +0900

    Add sentence analysis

--- programs/KoNLPy/run.py
+++ programs/KoNLPy/run.py
@@ -1,21 +1,23 @@
 import time
 import gtts
-import konlpy.tag
+import konlpy
+import nltk
 import pymysql
 
+speech = "유구한 역사와 전통에 빛나는 우리 대한국민은 3·1운동으로 건립된 대한민국임시정부의 법통과 불의에 항거한 4·19민주이념을 계승하고, 조국의 민주개혁과 평화적 통일의 사명에 입각하여 정의·인도와 동포애로써 민족의 단결을 공고히 하고, 모든 사회적 폐습과 불의를 타파하며, 자율과 조화를 바탕으로 자유민주적 기본질서를 더욱 확고히 하여 정치·경제·사회·문화의 모든 영역에 있어서 각인의 기회를 균등히 하고, 능력을 최고도로 발휘하게 하며, 자유와 권리에 따르는 책임과 의무를 완수하게 하여, 안으로는 국민생활의 균등한 향상을 기하고 밖으로는 항구적인 세계평화와 인류공영에 이바지함으로써 우리들과 우리들의 자손의 안전과 자유와 행복을 영원히 확보할 것을 다짐하면서 1948년 7월 12일에 제정되고 8차에 걸쳐 개정된 헌법을 이제 국회의 의결을 거쳐 국민투표에 의하여 개정한다. 1987년 10월 29일."
 
-def okt_token(text):
-    okt = konlpy.tag.Okt()
-    answer = ""
-    for word, pumsa in okt.pos(text, norm=True):
-        if pumsa in ["Josa", "PreEomi", "Eomi", "Suffix", "Punctuation"]:
-            answer += word
-        elif pumsa in ["Noun", "Verb", "Adjective", "Determiner", "Adverb", "Conjunction", "Exclamation"]:
-            answer += " " + word
-        else:
-            answer += " " + word
-    return answer
+grammar = """
+NP: {<NR><NNM><J.*>*}
+    {<NR>+<SP><NR>}
+    {<N.*>+<XSN>?<J.*>*}
+VP: {<V.*>+<E.*>?}
+    {<N.*><XSV><E.*><J.*>*}
+AP: {<A.*>*}
+MP: {<M.*>*}
+"""
 
+kkma = konlpy.tag.Kkma()
+okt = konlpy.tag.Okt()
 
 with open("password.txt", "r") as f:
     password = f.readline().strip()
@@ -26,5 +28,9 @@ with open("password.txt", "r") as f:
 # query = "SELECT * FROM `SpeechList` WHERE `Algorithm` LIKE 'kkma' AND `Status` LIKE 'wait' ORDER BY `UploadTime` ASC"
 # cursor.execute(query)
 
-tts = gtts.gTTS(okt_token(input("Enter:")), lang="ko-KR", lang_check=False)
-tts.save("/data/tmp.mp3")
+for sentence in kkma.sentences(speech):
+    words = kkma.pos(sentence)
+    parser = nltk.RegexpParser(grammar)
+    chunks = parser.parse(words)
+
+    chunks.pprint()
